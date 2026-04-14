5beadef04a819d81e23e68951561470a67d2c90d
bentegehlen <bentegehlen@gmail.com>
2026-04-14 22:46:56 +0200

    analyze-garment.js aktualisieren

--- netlify/functions/analyze-garment.js
+++ netlify/functions/analyze-garment.js
@@ -1,1 +1,18 @@
+exports.handler = async function(event) {
+  try {
+    const { imageDataUrl } = JSON.parse(event.body);
 
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Function läuft 🎉",
+        received: !!imageDataUrl
+      })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
